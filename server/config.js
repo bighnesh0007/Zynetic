a1e81280565970d8476c3645d41a77ff8b4576bf
bighnesh0007 <bigneshkumarsahoo58@gmail.com>
2025-03-29 23:38:26 +0530

    v2 - User Authentication done

--- server/config.js
+++ server/config.js
@@ -1,1 +1,5 @@
 JWT_User_Pass = "23ewd3@#83"
+
+module.exports={
+    JWT_User_Pass:JWT_User_Pass
+}
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -3,14 +3,51 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const { UserModel } = require('../db');
-const {JWT_User_Pass} =require('../config')
+const { JWT_User_Pass } = require('../config')
 
 const userRouter = Router();
 
 
+userRouter.post('/signup', async (req, res) => {
+    const { name, email, password, phone, address } = req.body;
+
+    try {
+        await UserModel.create({
+            name: name,
+            email: email,
+            password: password,
+            phone: phone,
+            address: address
+        });
+        res.status(201).json({ message: 'User created successfully' });
 
 
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ message: 'Error creating user' });
 
+    }
+})
+
+userRouter.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({
+            email: email,
+            password: password
+        })
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        const token = jwt.sign({ id: user._id }, JWT_User_Pass);
+
+        res.status(200).json({ message: 'User signed in successfully', token: token });
+
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ message: 'Error signing in' });
+    }
+})
 
 module.exports = {
     userRouter: userRouter
